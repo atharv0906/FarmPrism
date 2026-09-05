f81b1f8c121c2f3ac6b347f34f70a030dc0ca387
avkharat06 <avkharat061@gmail.com>
2026-09-05 22:50:25 +0530

    1.1.2.2: Fix Loader Animation & Navigation Bar

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -2,11 +2,13 @@ import { useEffect, useRef } from 'react';
 import {
   Animated,
   Easing,
+  Image,
   ImageBackground,
   StyleSheet,
   View,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const splashImage = require('../../assets/splash_reference.png');
 const DESIGN_WIDTH = 1080;
@@ -14,9 +16,10 @@ const DESIGN_HEIGHT = 2340;
 const DESIGN_ASPECT_RATIO = DESIGN_WIDTH / DESIGN_HEIGHT;
 
 export function SplashScreen() {
+  const insets = useSafeAreaInsets();
   const backgroundOpacity = useRef(new Animated.Value(0)).current;
   const backgroundScale = useRef(new Animated.Value(1.03)).current;
-  const loaderSweep = useRef(new Animated.Value(-1)).current;
+  const loaderPulse = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
     const backgroundAnimation = Animated.parallel([
@@ -36,15 +39,16 @@ export function SplashScreen() {
 
     const loaderAnimation = Animated.loop(
       Animated.sequence([
-        Animated.timing(loaderSweep, {
-          toValue: 1,
-          duration: 850,
-          easing: Easing.linear,
+        Animated.timing(loaderPulse, {
+          toValue: 1.04,
+          duration: 450,
+          easing: Easing.inOut(Easing.ease),
           useNativeDriver: true,
         }),
-        Animated.timing(loaderSweep, {
-          toValue: -1,
-          duration: 0,
+        Animated.timing(loaderPulse, {
+          toValue: 1,
+          duration: 450,
+          easing: Easing.inOut(Easing.ease),
           useNativeDriver: true,
         }),
       ]),
@@ -52,7 +56,7 @@ export function SplashScreen() {
 
     const timeline = Animated.sequence([
       backgroundAnimation,
-      Animated.delay(250),
+      Animated.delay(200),
     ]);
 
     timeline.start();
@@ -62,15 +66,10 @@ export function SplashScreen() {
       timeline.stop();
       loaderAnimation.stop();
     };
-  }, [backgroundOpacity, backgroundScale, loaderSweep]);
-
-  const loaderTranslate = loaderSweep.interpolate({
-    inputRange: [-1, 1],
-    outputRange: [-110, 110],
-  });
+  }, [backgroundOpacity, backgroundScale, loaderPulse]);
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingBottom: insets.bottom }]}>
       <StatusBar style="light" />
       <View style={styles.frame}>
         <Animated.View
@@ -89,15 +88,19 @@ export function SplashScreen() {
             style={styles.image}
           />
 
-          <Animated.View
-            pointerEvents="none"
-            style={[
-              styles.loaderSweep,
-              {
-                transform: [{ translateX: loaderTranslate }],
-              },
-            ]}
-          />
+          <View pointerEvents="none" style={styles.loaderMask}>
+            <Animated.Image
+              source={splashImage}
+              resizeMode="cover"
+              style={[
+                styles.loaderImage,
+                {
+                  transform: [{ scale: loaderPulse }],
+                  opacity: 1,
+                },
+              ]}
+            />
+          </View>
         </Animated.View>
       </View>
     </View>
@@ -116,6 +119,7 @@ const styles = StyleSheet.create({
     maxHeight: '100%',
     aspectRatio: DESIGN_ASPECT_RATIO,
     overflow: 'hidden',
+    paddingBottom: 0,
   },
   imageWrap: {
     width: '100%',
@@ -126,14 +130,16 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  loaderSweep: {
+  loaderMask: {
     position: 'absolute',
-    left: '24%',
-    right: '24%',
+    left: '22%',
+    right: '22%',
     bottom: '9.5%',
-    height: 12,
-    borderRadius: 999,
-    backgroundColor: 'rgba(255,255,255,0.18)',
-    opacity: 0.42,
+    height: 18,
+    overflow: 'hidden',
+  },
+  loaderImage: {
+    width: '100%',
+    height: '100%',
   },
 });
